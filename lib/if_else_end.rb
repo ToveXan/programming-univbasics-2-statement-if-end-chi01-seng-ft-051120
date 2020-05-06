83b495308fc9c958d157030c1fc19afd003d070d
Zoe Xanos <zxanos@gmail.com>
2020-05-06 05:12:39 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,7 +1,9 @@
 # Write your solution here
-run_code_inside = FALSE
+run_code_inside = false
 puts "Code before if...end"
 if run_code_inside
   puts "code inside"
 end
   puts "Code after if...end"
+
+chance_of_ran
