8723215c30503f5826f7baba8208fdfb4c350fcd
Zoe Xanos <zxanos@gmail.com>
2020-05-06 05:14:12 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -9,4 +9,8 @@ end
 chance_of_rain = 0.2
 puts "Let's go outside!"
 if chance_of_rain > 0.5
-  puts "Pack an umbrella"
+  puts "Pack an umbrella!"
+else
+  puts "Enjoy a fine day!"
+end
+  puts "Oh, and always wear sunscreen!"
